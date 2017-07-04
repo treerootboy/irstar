8d77d0353a58f0a8dafb7f497fde90625ff5fb9f
treerootboy <treerootboy@gmail.com>
2017-07-04 08:25:16 +0800

    1. add supervisor support 2. add component status api

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ IR_LIB = "/tmp/ir"
 
 from flask import Flask, jsonify
 import broadlink
+import time
 
 app = Flask(__name__)
 
@@ -20,6 +21,37 @@ def temperature():
     temp = device.check_temperature()
     return jsonify({ 'status': 'ok', 'temperature': temp})
 
+@app.route("/home/learn/<cmd>")
+def learn(cmd):
+    device.enter_learning()
+    timeout = time.time() + 30
+    ir_pack = None
+    while (ir_pack is None) and timeout > time.time():
+      ir_pack = device.check_data()
+      time.sleep(1)
+
+    # recieved ir_pack
+    if (ir_pack is not None):
+      with open("%s/%s.ir" % (IR_LIB, cmd), 'w') as f:
+        f.write(ir_pack)
+      return jsonify({ 'status': 'ok', 'message': 'command %s has learned' % (cmd) })
+    
+    # recieve timeout
+    if (timeout<time.time()):
+      return jsonify({ 'status': 'error', 'message': 'command recieve timeout'})  
+
+    # recieve empty command
+    return jsonify({ 'status': 'error', 'message': 'recieved command is empty'})  
+
+@app.route("/home/<dev>/status")
+def status(dev):
+  try:
+    with open("%s/%s.st" % (IR_LIB, dev)) as f:
+      status = f.read()
+    return 'ON' if status == 'ON' else 'OFF'
+  except:
+    return 'OFF'
+
 @app.route("/home/<dev>/<cmd>")
 def command(dev, cmd):
 #    try:
@@ -28,6 +60,16 @@ def command(dev, cmd):
 
       if ir_packet:
         device.send_data(ir_packet)
+        if cmd == 'open' or cmd == 'ON':
+          status = 'ON'
+          with open("%s/%s.st" % (IR_LIB, dev), 'w') as f:
+            f.write('ON')
+        elif cmd == 'close' or cmd == 'OFF':
+          status = 'OFF'
+          with open("%s/%s.st" % (IR_LIB, dev), 'w') as f:
+            f.write('ON')
+        
+        
         return jsonify({ 'status': 'ok', 'message': '%s %sed' % (dev, cmd)})
       
       return jsonify({ 'status': 'error', 'message': 'ir_packet is empty' })
@@ -35,23 +77,3 @@ def command(dev, cmd):
 #    except:
 #      return  jsonify({ 'status': 'error', 'message': 'server got an exception' })
 
-@app.route("/home/learnir/<cmd>")
-def learn(cmd):
-    device.enter_learning()
-    timeout = time.time() + 30
-    while (ir_pack is None) and timeout > time.time():
-      ir_pack = device.check_data()
-      time.sleep(1)
-
-    # recieved ir_pack
-    if (ir_pack is not None):
-      with open("%s/%s.ir" % (IR_LIB, cmd)) as f:
-        f.write(ir_pack)
-      return jsonify({ 'status': 'ok', 'message': 'command %s has learned' })
-    
-    # recieve timeout
-    if (timeout<time.time()):
-      return jsonify({ 'status': 'error', 'message': 'command recieve timeout'})  
-
-    # recieve empty command
-    return jsonify({ 'status': 'error', 'message': 'recieved command is empty'})  
--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo apt-get install python-dev supervisor node -y
 sudo pip install flask broadlink --upgrade
-sudo cp -f supervisor.conf /etc/supervisord/conf.d/
+sed "s|<dir>|`pwd`|" supervisord.conf > irstar.conf
+sudo mv -f irstar.conf /etc/supervisord/conf.d/
+sudo systemctl enable supervisor
+sudo systemctl start supervisor
 sudo supervisorctl start flask
 sudo npm install -g homebridge homebridge-http
 sudo cp homebridge.config.json ~/.homebridge/config.json
